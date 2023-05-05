b890ad7dfb8c2824b570327f6616d0a2dad16a7c
sbeou <sbeou13@gmail.com>
2023-05-05 10:02:55 -0300

    Fix bug test home

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,7 +4,7 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); })
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -15,8 +15,8 @@ const Form = ({ onSuccess, onError }) => {
       // We try to call mockContactApi
       try {
         await mockContactApi();
-        onSuccess(true);
-        setSending(false);        
+        setSending(false);
+        onSuccess(true);       
       } catch (err) {
         setSending(false);
         onError(err);
